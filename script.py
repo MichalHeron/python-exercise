9ae7f5212eb5b5821a154d0b77dc0d6633020d44
michal <michklak91@gmail.com>
2024-04-20 11:50:04 +0200

    sum of even numbers added

--- script.py
+++ script.py
@@ -91,4 +91,12 @@
 #     if is_prime:
 #          print(n)
 
-         ######
+         ###### sum of even numbers
+# sum = 0;
+# for n in range (1, 100):
+#     if n % 2 == 0:
+#         sum += n
+# print(sum)
+
+#########
+
